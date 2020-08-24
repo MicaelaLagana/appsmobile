c22b5de94d3aeb03c61d62a095286f0e7fe473f6
MicaelaLagana <lagana.mcl@gmail.com>
2020-08-24 20:01:03 -0300

    ejercicio tres

---
+++ ejercicio_tres/index.js
@@ -0,0 +1,9 @@
+function ejercicio4(numEntero) {
+    if (numEntero % 2 == 0){
+        return 'par';
+    }
+    return 'impar';
+}
+
+console.log("ejemplo retornando par: numero 4", ejercicio4(4));
+console.log("ejemplo retornando impar: numero 3 ", ejercicio4(3));

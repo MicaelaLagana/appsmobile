f6c1b48978d1beef5c2d763e12883ff43e8b05ed
MicaelaLagana <lagana.mcl@gmail.com>
2020-08-24 19:49:53 -0300

    ejercicio dos

---
+++ ejercicio_dos/index.js
@@ -0,0 +1,5 @@
+var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+for (let i in meses){
+    console.log(meses[i]);
+}
